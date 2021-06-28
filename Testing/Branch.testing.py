df0d3b2acac0a850578fd5244122d7c8681aa1fe
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-28 12:52:04 -0600

    Update Branch.testing.py

--- Testing/Branch.testing.py
+++ Testing/Branch.testing.py
@@ -7,5 +7,4 @@ os.system('cls')
 main = int(input("Enter a number: "))
 deloc = int(random.randint(0,255))
 
-print('RandInt: ' + str(deloc))
 print('Added ' + str(main) + ' to ' + str(deloc) + ' which gives us ' + str(int(main) + int(deloc)))
