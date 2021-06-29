4f9fe72973c192d104d0aa10924c30f2c088b850
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-29 09:17:58 -0600

    Update Branch.testing.py

--- Testing/Branch.testing.py
+++ Testing/Branch.testing.py
@@ -1,7 +1,6 @@
 import random
 import os
 import tweepy
-from tweepy import lib
 
 os.system('cls')
 
@@ -9,3 +8,5 @@ main = int(input("Enter a number: "))
 deloc = int(random.randint(0,256))
 
 print('Added ' + str(main) + ' to ' + str(deloc) + ' which gives us ' + str(int(main) + int(deloc)))
+
+tweepy.api.get_oembed.__dir__('iterable[str]')
