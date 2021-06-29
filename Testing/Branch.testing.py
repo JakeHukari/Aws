8f4207994e2922896712ab6c0d0b7a49d9511c62
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-29 09:15:02 -0600

    Update Branch.testing.py

--- Testing/Branch.testing.py
+++ Testing/Branch.testing.py
@@ -7,7 +7,3 @@ main = int(input("Enter a number: "))
 deloc = int(random.randint(0,256))
 
 print('Added ' + str(main) + ' to ' + str(deloc) + ' which gives us ' + str(int(main) + int(deloc)))
-
-if deloc: int('256')
-os.system('cls')
-print('Aborted')
