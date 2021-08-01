c73194d0dea5af1b26a75e67d7dc8aebbbbfbbff
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-07-31 22:48:37 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -185,6 +185,12 @@ $(function() {
   });
   
 document.addEventListener('keyup', (event) => {
-    if (event.key == 'ArrowUp') {     
+    if (event.key == 'ArrowUp') {
+      function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
     }
 });
