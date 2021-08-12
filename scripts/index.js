58bd8a6d905cc0bb4b783256f7b7c43d03bc299e
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-08-12 13:14:14 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -193,4 +193,36 @@ document.addEventListener('keyup', (event) => {
       }
     }
 });
+function seco(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function gecko(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function checko() {
+  let user = gecko("user");
+  if (user != "") {
+    alert("Altoid");
+  } else {
+     seco("useragent", navigator.userAgent, 30);
+     if (user != "" && user != null) {
+       seco("Fatal Error", "Please alert someone this is very critical", 30);
+     }
+  }
+}
 console.log(navigator.userAgent);
