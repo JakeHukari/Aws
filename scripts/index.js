dd3ac51e06b31b3ce4f37f66b908bfbebdff7348
Krypt0c <jake@hukari.tech>
2021-09-22 21:23:12 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -185,11 +185,5 @@ $(function() {
   
 document.addEventListener('keyup', (event) => {
     if (event.key == 'ArrowUp') {
-      function setCookie(cname, cvalue, exdays) {
-        const d = new Date();
-        d.setTime(d.getTime() + (exdays*24*60*60*1000));
-        let expires = "expires="+ d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-      }
     }
 });
